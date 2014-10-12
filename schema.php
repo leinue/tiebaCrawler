28528b19296a7a2d73e4ef750a69f78482a53156
ivy <597055914@qq.com>
2014-10-12 10:16:51 +0800

    update

--- schema.php
+++ schema.php
@@ -22,32 +22,67 @@ function matchOfEqualLength($depot,$input){
 		}
 	}
 
+	//计算某个字符串的标准值,比如fuck为0.5,bitch为0.4,这个视字符串长度而定
 	$repeat=1-ceil($ilength/2)/$ilength;
 
+	//$flag/count($input)表示在input中出现的和$depot中字符相同的概率
+	//如果大于等于repeat就判断为恶意词汇
 	return $flag/count($input)>=$repeat;
 }
 
-//echo matchOfEqualLength("fuckbitch","fxxkbxxxh");
-
-//打乱字母顺序
+/**
+* 打乱字母顺序
+* @param $depot 某词比如fuck
+* @return 返回字母打乱后的字符串数组
+*/
 function ruffle($depot){
 
 	$depot=str_split($depot);
 	$cnt=count($depot);
-
-	foreach ($depot as $key => $value) {
-		if($key!=$cnt-1){
-			$tmp=$depot[$key];
-			$depot[$key]=$depot[$key+1];
-			$depot[$key+1]=$tmp;
+	
+	for ($i=0; $i < $cnt; $i++) { 
+		foreach ($depot as $key => $value) {
+			if($key!=$cnt-1){
+				//交换变量,和冒泡排序差不多
+				$tmp=$depot[$key];
+				$depot[$key]=$depot[$key+1];
+				$depot[$key+1]=$tmp;
+			}
 		}
+		//生成新的数组,注意是字符数组
+		$arr[]=$depot;
 	}
-	foreach ($depot as $key => $value) {
-		$dtr.=$value;
+
+	//将字符数组转换为字符串数组
+	foreach ($arr as $key => $value) {
+		foreach ($value as $pos => $char) {
+			$word.=$char;
+		}
+		$new[]=$word;
+		$word="";
 	}
-	return $dtr;
+	return $new;
 }
 
-echo ruffle("fcuk");
+/**
+* 读入词库
+* @param 词库目录
+* @return 词库
+*/
+function loadDepot($dir){
+	$content = file_get_contents($dir);
+	return nl2br($content);
+}
+
+//这么用
+/*$r=ruffle("fcuk");
+foreach ($r as $key => $word) {
+	$t=matchOfEqualLength($word,"fcxk");//第一个参数是词库数据,每次填入一个词,数据多的时候加个循环就行
+	if($t){
+		echo 'fcxk match '.$word.' not valid<br>';
+	}else{
+		echo 'fcxk match '.$word.' valid<br>';
+	}
+}*/
 
 ?>
